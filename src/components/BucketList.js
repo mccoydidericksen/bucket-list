f2be719a1f61e9f0c0d476dd2852cb935b7bb3d2
SpectreKilo <johnho8788@gmail.com>
2023-03-02 16:24:48 -0500

    bucketlist changes

--- src/components/BucketList.js
+++ src/components/BucketList.js
@@ -9,7 +9,7 @@ function BucketList() {
   const addBucketItem = (item) => {
 
     // TODO: Write logic to add the new bucket item to the bucket state variable
-    
+    setBucket([...bucket, item]);
   };
 
   // Function to mark bucket list item as complete
@@ -18,7 +18,13 @@ function BucketList() {
     let updatedBucket = bucket.map((item) => {
       
       // TODO: Write logic that marks an item as complete or incomplete when invoked
-
+      if (item.id === id) {
+        return {
+        ...item,
+        completed: !item.completed
+        };
+        }
+      return item;
     });
 
     setBucket(updatedBucket);
@@ -27,9 +33,10 @@ function BucketList() {
   // Function to remove bucket list item and update state
   const removeBucketItem = (id) => {
     // TODO: Write logic that will return an array of items that don't contain the ID passed to this function
-
+    let filteredBucket = bucket.filter((item) => item.id !== id);
 
     // TODO: Update the bucket state variable
+    setBucket(filteredBucket);
   };
 
   // Function to edit the bucket list item
